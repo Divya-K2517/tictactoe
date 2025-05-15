928dae90839c1cfcd2e5f46bf565197229b14ae7
Divya Kumari <115424525+Divya-K2517@users.noreply.github.com>
2025-05-14 22:40:05 -0400

    improved backend-frontend connection

--- renderer.js
+++ renderer.js
@@ -1,15 +1,17 @@
 //frontend
-const { ipcRenderer } = require('electron');
-document.getElementById('fetchBtn').onclick = async () => {
-  const message = await ipcRenderer.invoke('get-message');
-  document.getElementById('output').innerText = message;
-};
+const{ ipcRenderer } = require ('electron');
+//will receive messages from main process
+ipcRenderer.on('message', (event, message) => {
+    console.log(message); //printing message to console
+});
 
 document.querySelectorAll('.Tile').forEach(tile => {
-    tile.addEventListener('click', function() {
+    tile.addEventListener('click', async function() {
       const spot = this.getAttribute('data-spot');
       console.log('Clicked spot:', spot);
-      // Here you can send the spot to the main process if needed:
-      ipcRenderer.invoke('make-move', spot);
+      //using await bc ipdRenderer itself returns a Promise
+      const [a, b] =  await ipcRenderer.invoke('make-move', spot);
+      //a is winner, and b is whether it is draw or not
+      console.log(a, b);
     });
   });
