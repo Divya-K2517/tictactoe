e82ea8f284379f01a63d5df28549b402d2ad1214
Divya Kumari <115424525+Divya-K2517@users.noreply.github.com>
2025-05-14 22:40:49 -0400

    improved backend-frontend connection

--- main.js
+++ main.js
@@ -3,19 +3,33 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path');
 const {Board} = require('./backend.js');
 let gameBoard = new Board();
+
+ipcMain.handle('make-move', (event, spot) => {
+    console.log('make move handler called with spot:', spot);
+    gameBoard.makeMove(spot);
+    const winner = gameBoard.checkForWin();
+    const isDraw = gameBoard.checkForDraw();
+    return [winner, isDraw];
+});
+ipcMain.handle('reset-game', () => {
+    gameBoard.reset();
+    board = gameBoard.tiles.map(tile => tile.getContent());
+    return board;
+});
 //creteWindow lods the webpage into a BrowserWindow instance
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      preload: path.join(__dirname, 'renderer.js')
-    }
-  });
-  win.loadFile('index.html')
-}
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      }
+    });
+    win.loadFile('index.html')
+    //sending message to renderer
+    win.webContents.send('message', "Hello from main.js!");
+  }
 //calls createWindow when ready
 app.whenReady().then(() => {
   createWindow()
