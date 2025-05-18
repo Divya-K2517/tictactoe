5e156f4f9ed1d0632bfd18ab3de544aa68a98260
Divya Kumari <115424525+Divya-K2517@users.noreply.github.com>
2025-05-18 17:32:00 -0400

    updated UI, fixed bugs

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 //frontend
+//tile color: F8DAEC
 const{ ipcRenderer } = require ('electron');
 //will receive messages from main process
 ipcRenderer.on('message', (event, message) => {
@@ -12,13 +13,22 @@ document.querySelectorAll('.Tile').forEach(tile => {
       //using await bc ipdRenderer itself returns a Promise
       const [winner, draw, computerMove] =  await ipcRenderer.invoke('make-move', spot);
        //if the user clicked on a spot that was not empty and an error was thrown 
-      if (winner == 9 && draw == 9 && computerMove == 9) {
+      if (winner === 9 && draw === 9 && computerMove === 9) {
         showErrorMessage();
       } else {
         console.log("winner:", winner, "draw?", draw, "computer move: ", computerMove);
         this.style.backgroundImage = "url(assets/X.png)";
         if (winner != 2 && !draw) {
             markComputerMove(computerMove);
+        }
+        if (winner == 2) {
+            //show user winner message
+            let userWinElement = document.querySelector('.userWin');
+            userWinElement.style.visibility = "visible";
+        } else if (winner == 1) {
+            //show computer winner message
+            let computerWinElement = document.querySelector('.computerWin');
+            computerWinElement.style.visibility = "visible";
         }
       }
     });
@@ -35,13 +45,18 @@ document.querySelectorAll('.Reset').forEach(resetButton => {
             const content = boardState[index];
             tile.style.backgroundImage = content === 0 ? "url(assets/Tile.png)" : (content === 1 ? "url(assets/O.png)" : "url(assets/X.png)");
         });
+        //hiding both win elements
+        let userWinElement = document.querySelector('.userWin');
+        userWinElement.style.visibility = "hidden";
+        let computerWinElement = document.querySelector('.computerWin');
+        computerWinElement.style.visibility = "hidden";
     });
 });
 //marking the move that the computer made with an O
 function markComputerMove(computerMove) {
     document.querySelectorAll(".Tile").forEach(tile => {
         if (tile.getAttribute('data-spot') == computerMove) {
-            tile.style.backgroundImage = "url(assets/O.jpeg)";
+            tile.style.backgroundImage = "url(assets/O.png)";
         }
     });
 }
